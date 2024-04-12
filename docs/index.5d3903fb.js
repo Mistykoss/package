01592af46ffae484bd9ff996f9e68b7f8755c463
DESKTOP-6L08L2B\MystT <toimaxmermer12345@gmail.com>
2024-04-12 14:33:50 -0600

    updated

--- docs/index.5d3903fb.js
+++ docs/index.5d3903fb.js
@@ -39113,7 +39113,7 @@ const TIMELINE = new Map([
             }, 0).add({
                 targets: items.carousel,
                 opacity: 1,
-                backgroundColor: "rgb(0 0 0 / 60%)"
+                backgroundColor: "rgba(0, 0, 0, 0.5);"
             }, 0);
         }
     ],
